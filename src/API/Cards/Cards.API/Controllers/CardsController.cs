a0b4d72329232fc5993ac10ed1cc90aeb804a6f3
Ameya Shinde <ameyashinde123@outlook.com>
2022-09-28 01:17:10 +0530

    Created a Cards Controller.

---
+++ src/API/Cards/Cards.API/Controllers/CardsController.cs
@@ -0,0 +1,87 @@
+﻿using Cards.API.Data;
+using Cards.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cards.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CardsController : ControllerBase
+    {
+        private readonly CardsDbContext _context;
+
+        public CardsController(CardsDbContext context)
+        {
+            _context = context;
+        }
+
+        // Get All Cards
+        [HttpGet]
+        public async Task<IActionResult> GetAllCards()
+        {
+            var cards = await _context.Cards.ToListAsync();
+
+            return Ok(cards);
+        }
+
+        // Get single card
+        [HttpGet("id:guid")]
+        [ActionName("GetCard")]
+        public async Task<IActionResult> GetCard([FromRoute] Guid id)
+        {
+            var card = await _context.Cards.FirstOrDefaultAsync(x => x.Id == id);
+            if (card != null)
+            {
+                return Ok(card);
+            }
+
+            return NotFound("Card not found.");
+        }
+
+        // Add card
+        [HttpPost]
+        public async Task<IActionResult> AddCard([FromBody] Card card)
+        {
+            card.Id = Guid.NewGuid();
+
+            await _context.Cards.AddAsync(card);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetCard), card.Id, card);
+        }
+
+        // Updating a card
+        [HttpPut("id:guid")]
+        public async Task<IActionResult> UpdateCard([FromRoute] Guid id, [FromBody] Card card)
+        {
+            var existingCard = await _context.Cards.FirstOrDefaultAsync(x => x.Id == id);
+            if (existingCard != null)
+            {
+                existingCard.CardHolderName = card.CardHolderName;
+                existingCard.CardNumber = card.CardNumber;
+                existingCard.ExpiryMonth = card.ExpiryMonth;
+                existingCard.ExpiryYear = card.ExpiryYear;
+                existingCard.CVV = card.CVV;
+                await _context.SaveChangesAsync();
+
+                return Ok(existingCard);
+            }
+            return NotFound("Card not found.");
+        }
+
+        // Delete a card
+        [HttpDelete("id:guid")]
+        public async Task<IActionResult> DeleteCard([FromRoute] Guid id)
+        {
+            var existingCard = await _context.Cards.FirstOrDefaultAsync(x => x.Id == id);
+            if (existingCard != null)
+            {
+                _context.Remove(existingCard);
+                await _context.SaveChangesAsync();
+                return Ok(existingCard);
+            }
+            return NotFound("Card not found.");
+        }
+    }
+}
